eb44ec922945824c7e34100a67293e59df28f09b
Germanna Rebouças <ger_manna@hotmail.com>
2022-08-07 14:59:38 -0300

    Create BD_Introdução.py

---
+++ BD_Introdução.py
@@ -0,0 +1,42 @@
+___________________________________
+
+ pip install mysql-connector-python
+
+____________________________________
+
+import mysql.connector
+
+
+#Conexão com banco de dados
+#Host = onde está o servidor, coloca link, ip...
+#user e password = login e senha p acessar banco de dados
+#Database = nome do banco de dados
+
+
+conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='admin',
+    database='produto'
+)
+
+cursor = conexao.cursor(
+
+)
+
+#Colocar aqui o CRUD
+
+
+
+#Lembrar de fechar TUDOOO o que vc abrir
+
+cursor.close()
+conexao.close()
+
+
+
+#Formatar automaticamente = seleciona texto + (CRTL+ALT+L)
+
+
+
+___________________________________________________________________________________________
