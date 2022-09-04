142e9dec624b0d50b76064ad5b28afdb435f4c17
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-04 13:21:43 -0300

    Delete Python_InterfaceGrafica03.py

--- Python_InterfaceGrafica03.py
+++
@@ -1,9 +0,0 @@
-#Download Qt Designer
-#File - Salvar (arquivo.ui)
-#Abrir cmd como administrador
-#pip install PyQt5-tools (Caso necessario)
-#Exibir os diretorios do windows
-#Digitar dir
-#Digitar cd PASTA-QUE-ARQUIVO-TA-SALVO
-#Digitar pyuic5 ANTIGO.ui -o NOVO.py    (Mas na pasta que ele ta salvo) (Comando p conversao do arquivo)
-#Abrir o arquivo no pycharm
