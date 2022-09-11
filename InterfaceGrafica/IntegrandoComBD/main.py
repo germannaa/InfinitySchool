de5e84bf79498b830cc2708afcc208b18501fb34
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-11 16:29:04 -0300

    Update main.py

--- InterfaceGrafica/IntegrandoComBD/main.py
+++ InterfaceGrafica/IntegrandoComBD/main.py
@@ -1,7 +1,21 @@
 
+
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QWidget, QVBoxLayout, QMainWindow, QLineEdit
+import Banco2
+
+def teste():
+   # print(f' Login: {txt_login.text()}')
+   # print(f' Senha: {txt_senha.text()}')
+    Banco2.verificaLoginSenha(txt_login.text(), txt_senha.text())
+   
+   if :
+       print("Logado com sucesso")
+   else:
+       lbl_error.setText('Não foi possivel conectar. Login e/ou senha inválida')
+       
+
 
 #""Constante""
 TAMANHO_FONTE = 20
@@ -31,13 +45,19 @@ txt_senha.setEchoMode(QLineEdit.Password)
 
 
 botao = QPushButton('Entrar')
+botao.setFont(font)
+botao.clicked.connect(teste)
 
+lbl_error = QLabel()
+#lbl_error = QLabel('Não foi possivel conectar. Login e/ou senha inválida')
+lbl_error.setStyleSheet('color:red')
 
 layout.addWidget(label)
 layout.addWidget(lbl_login)
 layout.addWidget(txt_login)
 layout.addWidget(lbl_senha)
 layout.addWidget(txt_senha)
+layout.addWidget(lbl_error)
 layout.addWidget(botao)
 
 base.setLayout(layout)
