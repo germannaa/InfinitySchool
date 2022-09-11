17960421dc109958b33f4b6971de0c6da2ded682
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-11 15:44:40 -0300

    Update main.py

--- InterfaceGrafica/IntegrandoComBD/main.py
+++ InterfaceGrafica/IntegrandoComBD/main.py
@@ -1,10 +1,11 @@
 
+
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont
-from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QWidget, QVBoxLayout, QMainWindow
+from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QWidget, QVBoxLayout, QMainWindow, QLineEdit
 
 #""Constante""
-TAMANHO_FONTE = 30
+TAMANHO_FONTE = 25
 
 
 layout = QVBoxLayout()
@@ -19,10 +20,28 @@ label = QLabel('Infinity School')
 label.setFont(font)
 label.setAlignment(Qt.AlignCenter)
 
+
+label2 = QLabel('Login')
+label2.setFont(font)
+label2.setAlignment(Qt.AlignCenter)
+
+txt_login = QLineEdit()
+
+label3 = QLabel('Senha')
+label2.setFont(font)
+label2.setAlignment(Qt.AlignCenter)
+
+txt_senha = QLineEdit()
+
 botao = QPushButton('Botão')
 botao.setFont(font)
 
+
 layout.addWidget(label)
+layout.addWidget(label2)
+layout.addWidget(txt_login)
+layout.addWidget(label3)
+layout.addWidget(txt_senha)
 layout.addWidget(botao)
 
 base.setLayout(layout)
