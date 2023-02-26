ee376535f2743f373264e1d1c90bd19c4224bc21
Germanna Rebouças <reboucasgermanna@gmail.com>
2023-02-26 16:17:33 -0300

    Add files via upload

--- Principio Basicos - JS/aula04.js
+++ Principio Basicos - JS/aula04.js
@@ -73,4 +73,93 @@ function Exercicios() {
     }
 }
 
-Exercicios()
+//Exercicios()
+
+//__________________________________________________________________________________________
+
+//Atividade 02
+
+
+
+function Quest06(nome){
+    console.log("Olá, ", nome)
+}
+
+function Quest07(lado){
+    area = lado*lado;
+    //alert(`A area do quadrado com lado de ${lado} é de ${area}.`)
+    console.log("A  area do quadrado com lado de ", lado, "é de ", area, "m2.")
+}
+
+//Quest07(10)
+
+
+function Quest08(base, altura){
+    areaT = (base*altura)/2
+    console.log("A area do triangulo com base de ", base, " e altura de ", altura, " é de ", areaT, " m2.")
+}
+
+//Quest08(10, 2)
+
+function Quest09(numero){
+    for (i=0; i<= numero; i++){
+        console.log(i)
+    }
+}
+//Quest09(10)
+
+function Quest10(numero){
+    for (i=0; i<= numero; i++){
+        if (i%2 == 0 )
+        console.log(i)
+    }
+}
+//Quest10(10)
+
+
+/*
+let nome1 = prompt("Digite seu nome");
+Quest06(nome1)
+
+let numero = prompt("Digite um numero");
+
+Quest07(numero)
+Quest08(numero, numero)
+Quest09(numero)
+Quest10(numero)
+
+*/
+
+
+function Quest11(nota1, nota2){
+    media = (nota1+nota2)/2
+
+    if (media>=6){
+        return "Aprovado"
+    } else {
+        return "Reprovado"
+    }
+}
+
+//console.log(Quest11(5,5))
+
+function Quest12(num1,num2,num3){
+    if (num1>=num2 && num1>=num3){
+        return (num1)
+    } else if (num2>=num3){
+        return (num2)
+    } else {
+        return (num3)
+    }
+    }
+console.log(Quest12(1,2,10))
+
+function Quest13(idade,cnh){
+    if(idade<18 || cnh==false){
+        return ("Vc nao pode dirigir")
+    } else {
+        return("Vc pode dirigir")
+    }
+}
+
+//console.log(Quest13(8, true))
